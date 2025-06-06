c968b7b87a916ad5f7ece156b5ef568065ad3119
lucas-devworks <lucasteixeirasantos74@gmail.com>
2025-06-06 19:59:25 -0300

    Versão atualizada com recurso de botão para download

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ const container = document.querySelector('.container'),
 qrInput = container.querySelector('.conteudo input'),
 generateBtn = container.querySelector('.conteudo button'),
 qrImg = container.querySelector('.area-qr-code .imagem');
+qrdow = container.querySelector('.area-qr-code .dow');
 
 generateBtn.addEventListener('click', () => {
     let qrValue = qrInput.value;
     if (!qrValue) {
-        alert("Insira uma URL ou texto para gerar o qr code")
+        alert("Insira uma URL ou texto para gerar o qr code");
         return;
     }
     generateBtn.innerText = 'Gerando um Qr code...';
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    let qrCodeURL = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+
+    // Atualiza a imagem
+    qrImg.src = qrCodeURL;
     qrImg.addEventListener('load', () => {
         generateBtn.innerText = 'Gerar QR CODE';
         container.classList.add('active');
+
+        // Cria um link de download pelo Blob
+        fetch(qrCodeURL)
+            .then(response => response.blob())
+            .then(blob => {
+                let blobURL = URL.createObjectURL(blob);
+                qrdow.href = blobURL;
+                qrdow.download = "qrcode.png";
+            });
     });
 });
 
